d56a5406839d04f9416c28c647c35893978587f6
Dbirtic <Dominik.birtic666@gmail.com>
2021-03-18 20:32:25 +0100

    getElementsByClassName, TagName and so on

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+// Examine elements inside a document
 console.dir(document);
+
+// selectors
+
+const header = document.getElementById('header-title');
+
+console.log(header);
+
+/*header.textContent = "hello";
+header.innerText = "Goodbye";
+console.log(header.textContent); */
+
+header.style.borderBottom = "solid 3px #000";
+
+const headerDash = document.getElementById("main-header");
+headerDash.style.borderBottom = "solid 5px #777";
+
+// getElementsByClassName
+
+const listItems = document.getElementsByClassName("list-group-item");
+listItems[3].textContent = "LOL";
+listItems[1].style.backgroundColor = "#FF0";
+
+for(let i = 0; i < listItems.length; i++)
+{
+    listItems[i].style.fontSize = "20px";
+}
+
+// getElementsByTagName
+const li = document.getElementsByTagName("li");
+
